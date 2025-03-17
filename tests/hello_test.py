bae37e6d8f951be0c6819b96475d8418be5731e9
Robby Zhao <robby.zhaox@gmail.com>
2025-03-17 17:02:25 +0800

    update

--- tests/hello_test.py
+++ tests/hello_test.py
@@ -1,15 +1,10 @@
-import pytest
-
-from myocr.processors import OcrDetectionProcessor
-
-
 def test_hello():
     print("Hello, World!")
 
 
-@pytest.mark.skip
-def test_processor():
-    image = "/home/robby/code/myocr/tests/123.png"
-    p = OcrDetectionProcessor()
-    # img, _ =p.dbnet.load_image(image, detection_size=100)
-    p.process(image)
+# @pytest.mark.skip
+# def test_processor():
+#     image = "/home/robby/code/myocr/tests/123.png"
+#     p = OcrDetectionProcessor()
+#     # img, _ =p.dbnet.load_image(image, detection_size=100)
+#     p.process(image)
