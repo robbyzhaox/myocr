4d06c26ec8dfa30b815ab11204424e1a6b31a997
Robby Zhao <robby.zhaox@gmail.com>
2025-03-25 17:49:28 +0800

    fix

--- tests/hello_test.py
+++ tests/hello_test.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 from PIL import Image
 
 from myocr.models.model import ModelZoo
@@ -12,6 +13,6 @@ def test_hello():
 @pytest.mark.parametrize("iteration", range(1))
 def test_model(iteration):
 
-    model = ModelZoo.load_model("pt", "resnet18", "cuda:0")
+    model = ModelZoo.load_model("pt", "resnet18", "cuda:0" if torch.cuda.is_available() else "cpu")
     p = model.predictor(ImageClassificationParamConverter())
     print(p.predict(Image.open("/home/robby/code/myocr/tests/flower.png").convert("RGB")))
