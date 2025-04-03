e320884e31cba254f83695c1e18e9be8a19b4579
Robby Zhao <robby.zhaox@gmail.com>
2025-04-04 00:10:55 +0800

    update

--- myocr/config.py
+++ myocr/config.py
@@ -6,7 +6,11 @@ import yaml  # type: ignore
 # os.environ["LRU_CACHE_CAPACITY"] = "1"
 
 BASE_PATH = os.path.dirname(__file__)
-MODEL_PATH = os.environ.get("MYOCR_MODEL_PATH") or os.path.expanduser("~/.MyOCR/models/")
+MODEL_PATH = (
+    os.environ.get("MYOCR_MODEL_PATH")
+    or os.path.expanduser("~/.MyOCR/models/")
+    or os.path.expanduser("./models")
+)
 
 with open(BASE_PATH + "/logging_config.yaml", "r") as f:
     config = yaml.safe_load(f.read())
