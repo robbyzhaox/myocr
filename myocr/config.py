c59959982879cf785ee54b002baf17c09c2c7f18
Robby Zhao <robby.zhaox@gmail.com>
2025-04-04 08:37:40 +0800

    format

--- myocr/config.py
+++ myocr/config.py
@@ -8,15 +8,17 @@ import yaml  # type: ignore
 BASE_PATH = os.path.dirname(__file__)
 WORK_DIR = os.path.dirname(BASE_PATH)
 
+
 def get_model_path() -> str:
     if "MYOCR_MODEL_PATH" in os.environ and os.environ["MYOCR_MODEL_PATH"]:
         return os.environ["MYOCR_MODEL_PATH"]
-    
+
     user_path = os.path.expanduser("~/.MyOCR/models/")
     if os.path.exists(user_path):
         return str(user_path)
-    
-    return str(os.path.join(WORK_DIR,"models/"))
+
+    return str(os.path.join(WORK_DIR, "models/"))
+
 
 MODEL_PATH = get_model_path()
 
