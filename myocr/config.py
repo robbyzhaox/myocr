68c8cfad57c787dc77369030be7a6d51d8ac07d2
Robby Zhao <robby.zhaox@gmail.com>
2025-04-04 08:34:51 +0800

    working path

--- myocr/config.py
+++ myocr/config.py
@@ -6,11 +6,19 @@ import yaml  # type: ignore
 # os.environ["LRU_CACHE_CAPACITY"] = "1"
 
 BASE_PATH = os.path.dirname(__file__)
-MODEL_PATH = (
-    os.environ.get("MYOCR_MODEL_PATH")
-    or os.path.expanduser("~/.MyOCR/models/")
-    or os.path.expanduser("./models")
-)
+WORK_DIR = os.path.dirname(BASE_PATH)
+
+def get_model_path() -> str:
+    if "MYOCR_MODEL_PATH" in os.environ and os.environ["MYOCR_MODEL_PATH"]:
+        return os.environ["MYOCR_MODEL_PATH"]
+    
+    user_path = os.path.expanduser("~/.MyOCR/models/")
+    if os.path.exists(user_path):
+        return str(user_path)
+    
+    return str(os.path.join(WORK_DIR,"models/"))
+
+MODEL_PATH = get_model_path()
 
 with open(BASE_PATH + "/logging_config.yaml", "r") as f:
     config = yaml.safe_load(f.read())
